6dda1db9e68960a14ed72175bb1c2c87934106b5
Dhinesh <crdhinesh256@gmail.com>
2024-06-11 23:38:29 +0530

    Appointment drag and drop initial setup

--- src/features/Business/Schedule/DayCalender.tsx
+++ src/features/Business/Schedule/DayCalender.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { range, addDateBy, getMonday } from "./utils";
+import { range, addDateBy, getMonday, addTime } from "./utils";
 import { DayWrapper, HGrid, Hour, VGrid, HOUR_HEIGHT, HOUR_MARGIN_TOP, Appointment, Wrapper, HourLineWithLabel } from './components/CalenderComponents'
 import GetIcon from "../../../assets/Icon/icon";
 import { GetScheduleDates } from "./BusinessScheduleContext";
@@ -7,11 +7,61 @@ import { GetScheduleDates } from "./BusinessScheduleContext";
 export const DayCalendar = () => {
 
   const { filteredAppointments, employees } = GetScheduleDates()
+
+  const [appointments, setAppointments] = useState(filteredAppointments)
   
   const mondayDate = getMonday();
-  const appointments = filteredAppointments;
 
   console.log("appointment day", appointments)
+
+  const onDragStart = (e, id, currentEmployee) => {
+    e.dataTransfer.setData('eventId', id);
+    e.dataTransfer.setData('currentEmployee', currentEmployee);
+    const rectTop = e.target.getBoundingClientRect().top;
+
+    e.dataTransfer.setData('initialTop', rectTop);
+    console.log("updated time onDragStart", rectTop, id)
+
+  };
+
+  const onDragOver = (e) => {
+    e.preventDefault();
+  };
+
+  const onDrop = (e, newEmployee) => {
+    const id = e.dataTransfer.getData('eventId');
+    const currentEmployee = e.dataTransfer.getData('currentEmployee');
+
+    const initialTop = e.dataTransfer.getData('initialTop');
+
+    const finalTop = e.target.getBoundingClientRect().top;
+    const minutesPerPixel = 0.5;
+    const updatedMinutes = Math.round((initialTop - finalTop) * minutesPerPixel);
+
+    console.log("updated time onDrop", updatedMinutes, initialTop, finalTop, newEmployee)
+
+    const updatedAppointments = appointments
+
+    updatedAppointments.map(empAppointment => {
+
+      if (empAppointment.employee === newEmployee) {
+        empAppointment.appointments.map((app) => {
+          if(app.text === id) {
+            const newTime = addTime(app.date, 'minutes', updatedMinutes * -1)
+            console.log("newTime >>", newTime)
+            return { ...app, date: newTime };
+          }
+         return app
+        })
+      }
+      else {
+        //appointment[]
+      }
+      return empAppointment;
+    });
+
+    // setAppointments(updatedAppointments);
+  };
 
   return (
     <>
@@ -30,6 +80,8 @@ export const DayCalendar = () => {
             {employees ? 
             employees.map((employee, index) => (
                 <DayWrapper
+                  onDragOver={onDragOver}
+                  onDrop={(e) => onDrop(e, employee.employeeName)}
                   //onDoubleClick={() => onAddEvent(addDateBy(mondayDate, index))}
                 >
                   <div className="h-[120px] flex flex-col flex-grow-0 items-center justify-center font-bold border-b border-b-gray-400">
@@ -49,6 +101,10 @@ export const DayCalendar = () => {
                       // areDatesSame(addDateBy(mondayDate, index), appointment.date) && 
                     (
                         <Appointment
+                          draggable
+                          onDragStart = {(e) => {
+                            onDragStart(e, appointment.text, employee)
+                          }}
                           statusColor={appointment.status}
                           howLong={appointment.howLong}
                           fromTop={
