5e17517972ac42fd2dfe054a900996d8baf65255
vamsi <156523177+venkata-vamsi@users.noreply.github.com>
2024-06-13 22:39:53 +0530

    fix

--- src/features/Business/SalonProfile/SalonProfileSteps/Photos.tsx
+++ src/features/Business/SalonProfile/SalonProfileSteps/Photos.tsx
@@ -34,14 +34,19 @@ export const Photos = () => {
           dragProps
         }) => (
           // write your building UI
-          <div className="upload__image-wrapper">
-            {/* <button
-              style={isDragging ? { color: "red" } : null}
-              onClick={onImageUpload}
-              {...dragProps}
-            >
-              Click or Drop here
-            </button> */}
+          <div className="flex justify-center">
+            {imageList.map((image, index) => (
+              <div key={index} className="image-item">
+                <Card style={{width: '200px', height: '200px'}}>
+                    <img src={image.data_url} alt=""/>
+                </Card>
+                <div className="image-item__btn-wrapper">
+                  <button onClick={() => onImageUpdate(index)}>Update</button>
+                  <button onClick={() => onImageRemove(index)}>Remove</button>
+                </div>
+              </div>
+            ))}
+            
             <Card style={{width: '200px', height: '200px'}}>
               <CardContent sx={{marginTop: '30px'}}>
                   <button
@@ -56,101 +61,10 @@ export const Photos = () => {
             </Card>
             &nbsp;
             {/* <button onClick={onImageRemoveAll}>Remove all images</button> */}
-            {imageList.map((image, index) => (
-              <div key={index} className="image-item">
-                <Card style={{width: '200px', height: '200px'}}>
-                    <img src={image.data_url} alt=""/>
-                </Card>
-                <div className="image-item__btn-wrapper">
-                  <button onClick={() => onImageUpdate(index)}>Update</button>
-                  <button onClick={() => onImageRemove(index)}>Remove</button>
-                </div>
-              </div>
-            ))}
+            
           </div>
         )}
       </ImageUploading>
     </div>
   );
 };
-
-
-// const [images, setImages] = useState([]);
-
-// const handleImageChange = (e) => {
-//   const selectedImages = Array.from(e.target.files);
-//   setImages((prevImages) => [...prevImages, ...selectedImages]);
-// };
-
-// const handleDeleteImage = (index) => {
-//   setImages((prevImages) => {
-//     const updatedImages = [...prevImages];
-//     updatedImages.splice(index, 1);
-//     return updatedImages;
-//   });
-// };
-
-// const handleDragOver = (e) => {
-//   e.preventDefault();
-// };
-
-// const handleDrop = (e) => {
-//   e.preventDefault();
-//   const droppedFiles = Array.from(e.dataTransfer.files);
-//   setImages((prevImages) => [...prevImages, ...droppedFiles]);
-// };
-
-// const handleDragStart = (e, index) => {
-//   e.dataTransfer.setData('index', index);
-// };
-
-// const handleDragOverImage = (e) => {
-//   e.preventDefault();
-// };
-
-// const handleDropImage = (e, currentIndex) => {
-//   e.preventDefault();
-//   const draggedIndex = e.dataTransfer.getData('index');
-//   const draggedImage = images[draggedIndex];
-
-//   const newImages = images.filter((image, index) => index !== parseInt(draggedIndex));
-//   newImages.splice(currentIndex, 0, draggedImage);
-
-//   setImages(newImages);
-// };
-
-// return (
-//   <div>
-//     <div>
-//       <input
-//         type="file"
-//         accept="image/*"
-//         multiple
-//         onChange={handleImageChange}
-//         style={{ position: 'absolute', opacity: 0, width: '100%', height: '100%', cursor: 'pointer' }}
-//       />
-//             <GetIcon iconName='PlusIcon'/>
-
-//     </div>
-    
-//     <div
-//       onDragOver={handleDragOver}
-//       onDrop={handleDrop}
-//       style={{ border: '2px dashed #ccc', padding: '10px', marginTop: '10px' }}
-//     >
-//       {images.map((image, index) => (
-//         <div
-//           key={index}
-//           draggable
-//           onDragStart={(e) => handleDragStart(e, index)}
-//           onDragOver={(e) => handleDragOverImage(e)}
-//           onDrop={(e) => handleDropImage(e, index)}
-//           style={{ marginBottom: '10px', cursor: 'move' }}
-//         >
-//           <img src={URL.createObjectURL(image)} alt={`Image ${index}`} />
-//           <button onClick={() => handleDeleteImage(index)}>Delete</button>
-//         </div>
-//       ))}
-//     </div>
-//   </div>
-// );
