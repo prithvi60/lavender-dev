6539ce889f319944e1c026f5ce952dd801a823e5
vamsi <156523177+venkata-vamsi@users.noreply.github.com>
2024-07-05 01:57:13 +0530

    appointment scroll add

--- src/features/CustomerProfile/PastBookings.tsx
+++ src/features/CustomerProfile/PastBookings.tsx
@@ -1,55 +1,89 @@
-import { Card, Grid } from '@mui/material'
-import React, { useState } from 'react'
+import { Card, Grid, IconButton } from '@mui/material';
+import React, { useRef, useState } from 'react';
 import EmptyBookings from './EmptyBookings';
 import { NoPastBookings } from './Constant';
 import { BookingInfoModal } from './BookingInfoModal';
 import { convertToDateOnly, convertToDayOnly, convertToMonthOnly, convertToTimeOnly, convertToYearOnly } from '../../utils/TimeFormat';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
-function PastBookings({userInfo}) {
-
+function PastBookings({ userInfo }) {
     const [isModalOpen, setIsModalOpen] = useState(false);
+    const scrollContainerRef = useRef(null);
 
-    function handleCardClick () {
-        setIsModalOpen((prev) => !prev);
-    }
+    const handleCardClick = () => {
+        setIsModalOpen(!isModalOpen);
+    };
 
-  return (
-    <div className='mt-10'>
-        <div className='text-3xl font-bold py-4'>Past Bookings</div>
-        {
-            userInfo?.pastBookings?.length > 0 ? (
-            
-            userInfo?.pastBookings?.map((bookings)=>(
-                <div>
-                    <Card sx={{width: 500, height: 150, borderRadius: 4, cursor: 'pointer'}} onClick={()=>{handleCardClick()}}>
-
-                        <Grid container spacing={1}>
-                            <Grid item xs={4}>
-                                <Card sx={{height: 150, borderRadius: 4, backgroundColor: '#EEEEFF', color: '#1B1464', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
-                                    <div className="text-xl p-1">{convertToMonthOnly(bookings?.startTime)} {convertToYearOnly(bookings?.startTime)}</div>
-                                    <div className='text-5xl font-bold p-1'>{convertToDateOnly(bookings?.startTime)}</div>
-                                    <div className="text-xl p-1">{convertToDayOnly(bookings?.startTime)}</div>
-                                </Card>
-                            </Grid>
-                            <Grid item xs={8} sx={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'start'}}>
-                                <div className="text-xl p-1">{bookings?.establishmentName}</div>
-                                <div className="text-3xl font-bold p-1">{convertToTimeOnly(bookings?.startTime)}</div>
-                                <div className="text-xl p-1">{bookings.services.length} services</div>
-                            </Grid>
-                        </Grid>
-                    </Card> 
-            <br></br>
-            {isModalOpen && <BookingInfoModal isModalOpen={isModalOpen} bookings={bookings}/>}
-
-            </div>
-            ))
-        ) :
-        (
-            <EmptyBookings noAppointmentsMessage={NoPastBookings}/>
-        )
+    const scrollLeft = () => {
+        if (scrollContainerRef.current) {
+            scrollContainerRef.current.scrollLeft -= 500; // Adjust scroll amount as per your card width
         }
-    </div>
-  )
+    };
+
+    const scrollRight = () => {
+        if (scrollContainerRef.current) {
+            scrollContainerRef.current.scrollLeft += 500; // Adjust scroll amount as per your card width
+        }
+    };
+
+    return (
+        <div className='mt-10'>
+            <div className='text-3xl font-bold py-4'>Past Bookings</div>
+            <div style={{ overflow: 'hidden', position: 'relative' }}>
+                {userInfo?.pastBookings?.length > 0 ? (
+                    <>
+                        <div ref={scrollContainerRef} style={{ display: 'flex', flexDirection: 'row', transition: '0.5s', overflowX: 'auto' }}>
+                            {userInfo?.pastBookings?.map((bookings, index) => (
+                                <Card
+                                    key={index}
+                                    sx={{ minWidth: 500, maxWidth: 500, height: 150, borderRadius: 4, marginLeft: 2, marginRight: 2, cursor: 'pointer' }}
+                                    onClick={handleCardClick}
+                                >
+                                    <Grid container spacing={1}>
+                                        <Grid item xs={4}>
+                                            <Card
+                                                sx={{
+                                                    height: 150,
+                                                    borderRadius: 4,
+                                                    backgroundColor: '#EEEEFF',
+                                                    color: '#1B1464',
+                                                    display: 'flex',
+                                                    flexDirection: 'column',
+                                                    justifyContent: 'center',
+                                                    alignItems: 'center',
+                                                }}
+                                            >
+                                                <div className='text-xl p-1'>
+                                                    {convertToMonthOnly(bookings?.startTime)} {convertToYearOnly(bookings?.startTime)}
+                                                </div>
+                                                <div className='text-5xl font-bold p-1'>{convertToDateOnly(bookings?.startTime)}</div>
+                                                <div className='text-xl p-1'>{convertToDayOnly(bookings?.startTime)}</div>
+                                            </Card>
+                                        </Grid>
+                                        <Grid item xs={8} sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'start' }}>
+                                            <div className='text-xl p-1'>{bookings?.establishmentName}</div>
+                                            <div className='text-3xl font-bold p-1'>{convertToTimeOnly(bookings?.startTime)}</div>
+                                            <div className='text-xl p-1'>{bookings.services.length} services</div>
+                                        </Grid>
+                                    </Grid>
+                                </Card>
+                            ))}
+                        </div>
+                        <IconButton onClick={scrollLeft} style={{ position: 'absolute', left: 0, top: '50%', transform: 'translateY(-50%)', zIndex: 1 }}>
+                            <ArrowBackIcon />
+                        </IconButton>
+                        <IconButton onClick={scrollRight} style={{ position: 'absolute', right: 0, top: '50%', transform: 'translateY(-50%)', zIndex: 1 }}>
+                            <ArrowForwardIcon />
+                        </IconButton>
+                    </>
+                ) : (
+                    <EmptyBookings noAppointmentsMessage={NoPastBookings} />
+                )}
+                {isModalOpen && <BookingInfoModal isModalOpen={isModalOpen} bookings={userInfo?.pastBookings[0]} />}
+            </div>
+        </div>
+    );
 }
 
-export default PastBookings
+export default PastBookings;
--- src/features/CustomerProfile/UpComingBookings.tsx
+++ src/features/CustomerProfile/UpComingBookings.tsx
@@ -1,60 +1,89 @@
-import { Card, Grid } from '@mui/material'
-import React, { useState } from 'react'
+import { Card, Grid, IconButton } from '@mui/material';
+import React, { useRef, useState } from 'react';
 import EmptyBookings from './EmptyBookings';
 import { NoUpComingBookings } from './Constant';
 import { BookingInfoModal } from './BookingInfoModal';
 import { convertToDateOnly, convertToDayOnly, convertToMonthOnly, convertToTimeOnly, convertToYearOnly } from '../../utils/TimeFormat';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 
-function UpComingBookings({userInfo}) {
-
+function UpComingBookings({ userInfo }) {
     const [isModalOpen, setIsModalOpen] = useState(false);
-    
-    function handleCardClick () {
-        setIsModalOpen((prev) => !prev);
-    }
+    const scrollContainerRef = useRef(null);
 
-  return (
-    <div className='mt-10'>
-        <div className='text-3xl font-bold py-4'>Upcoming Bookings</div>
-        {userInfo?.upcomingBookings?.length > 0 ? (
-            userInfo?.upcomingBookings?.map((bookings)=>(
-            <div>
-                <Card sx={{width: 500, height: 150, borderRadius: 4}} onClick={()=>{handleCardClick()}}>
-                    <Grid container spacing={1}>
-                        <Grid item xs={4}>
-                            <Card sx={{height: 150, borderRadius: 4, backgroundColor: '#EEEEFF', color: '#1B1464', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
-                                <div className="text-xl p-1">{convertToMonthOnly(bookings?.startTime)} {convertToYearOnly(bookings?.startTime)}</div>
-                                <div className='text-5xl font-bold p-1'>{convertToDateOnly(bookings?.startTime)}</div>
-                                <div className="text-xl p-1">{convertToDayOnly(bookings?.startTime)}</div>
-                            </Card>
-                        </Grid>
-                        <Grid item xs={8} sx={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'start'}}>
-                            <div className="text-xl p-1">{bookings?.establishmentName}</div>
-                            <div className="text-3xl font-bold p-1">{convertToTimeOnly(bookings?.startTime)}</div>
-                            <div className="text-xl p-1">{bookings.services.length} services</div>
-                        </Grid>
-                    </Grid>
-                </Card>
-                <br></br>
-                {isModalOpen && <BookingInfoModal isModalOpen={isModalOpen} bookings={bookings}/>}
+    const handleCardClick = () => {
+        setIsModalOpen(!isModalOpen);
+    };
+
+    const scrollLeft = () => {
+        if (scrollContainerRef.current) {
+            scrollContainerRef.current.scrollLeft -= 500; // Adjust scroll amount as per your card width
+        }
+    };
+
+    const scrollRight = () => {
+        if (scrollContainerRef.current) {
+            scrollContainerRef.current.scrollLeft += 500; // Adjust scroll amount as per your card width
+        }
+    };
+
+    return (
+        <div className='mt-10'>
+            <div className='text-3xl font-bold py-4'>Upcoming Bookings</div>
+            <div style={{ overflow: 'hidden', position: 'relative' }}>
+                {userInfo?.upcomingBookings?.length > 0 ? (
+                    <>
+                        <div ref={scrollContainerRef} style={{ display: 'flex', flexDirection: 'row', transition: '0.5s', overflowX: 'auto' }}>
+                            {userInfo?.upcomingBookings?.map((bookings, index) => (
+                                <Card
+                                    key={index}
+                                    sx={{ minWidth: 500, maxWidth: 500, height: 150, borderRadius: 4, marginLeft: 2, marginRight: 2 }}
+                                    onClick={handleCardClick}
+                                >
+                                    <Grid container spacing={1}>
+                                        <Grid item xs={4}>
+                                            <Card
+                                                sx={{
+                                                    height: 150,
+                                                    borderRadius: 4,
+                                                    backgroundColor: '#EEEEFF',
+                                                    color: '#1B1464',
+                                                    display: 'flex',
+                                                    flexDirection: 'column',
+                                                    justifyContent: 'center',
+                                                    alignItems: 'center',
+                                                }}
+                                            >
+                                                <div className='text-xl p-1'>
+                                                    {convertToMonthOnly(bookings?.startTime)} {convertToYearOnly(bookings?.startTime)}
+                                                </div>
+                                                <div className='text-5xl font-bold p-1'>{convertToDateOnly(bookings?.startTime)}</div>
+                                                <div className='text-xl p-1'>{convertToDayOnly(bookings?.startTime)}</div>
+                                            </Card>
+                                        </Grid>
+                                        <Grid item xs={8} sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'start' }}>
+                                            <div className='text-xl p-1'>{bookings?.establishmentName}</div>
+                                            <div className='text-3xl font-bold p-1'>{convertToTimeOnly(bookings?.startTime)}</div>
+                                            <div className='text-xl p-1'>{bookings.services.length} services</div>
+                                        </Grid>
+                                    </Grid>
+                                </Card>
+                            ))}
+                        </div>
+                        <IconButton onClick={scrollLeft} style={{ position: 'absolute', left: 0, top: '50%', transform: 'translateY(-50%)', zIndex: 1 }}>
+                            <ArrowBackIcon />
+                        </IconButton>
+                        <IconButton onClick={scrollRight} style={{ position: 'absolute', right: 0, top: '50%', transform: 'translateY(-50%)', zIndex: 1 }}>
+                            <ArrowForwardIcon />
+                        </IconButton>
+                    </>
+                ) : (
+                    <EmptyBookings noAppointmentsMessage={NoUpComingBookings} />
+                )}
+                {isModalOpen && <BookingInfoModal isModalOpen={isModalOpen} bookings={userInfo?.upcomingBookings[0]} />}
             </div>
-            // <div className="upcoming-details flex flex-wrap border border-[#CCCCCC] rounded-[20px]">
-            //     <div className="text-[#1B1464] bg-[#EEEEFF] text-center gap-2 p-8 aspect-square grid rounded-[20px]">
-            //     <span className='font-medium'>{convertToMonthOnly(bookings?.startTime)} {convertToYearOnly(bookings?.startTime)}</span>
-            //     <h2 className='text-6xl font-semibold'>{convertToDateOnly(bookings?.startTime)}</h2>
-            //     <span className='font-medium'>{convertToDayOnly(bookings?.startTime)}</span>
-            //     </div>
-            //     <div className="text-[#4D4D4D] gap-2 p-8 grid">
-            //     <span className='font-medium'>{bookings?.establishmentName}</span>
-            //     <h2 className='text-6xl font-semibold'>{convertToTimeOnly(bookings?.startTime)}</h2>
-            //     <span className='font-medium'>{bookings.services.length} services</span>
-            //     </div>
-            // </div>
-            ))
-        ) : <EmptyBookings noAppointmentsMessage={NoUpComingBookings}/>}
-        
-    </div>
-  )
+        </div>
+    );
 }
 
-export default UpComingBookings
+export default UpComingBookings;
