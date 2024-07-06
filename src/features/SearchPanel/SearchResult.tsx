a0aca3b4a8d5e67d48bc705d522705bb5dbe70d3
vamsi <156523177+venkata-vamsi@users.noreply.github.com>
2024-07-06 14:10:09 +0530

    card design fix

--- src/features/SearchPanel/SearchResult.tsx
+++ src/features/SearchPanel/SearchResult.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import Chip from '../../components/Chip';
-import { Grid, Card, CardContent, Rating, CardActions, CardHeader, styled, TextField, Typography } from '@mui/material';
+import { Grid, Card, CardContent, Rating, CardActions, CardHeader, styled, Typography } from '@mui/material';
 import FilterModal from '../../components/FilterModal';
 import { useSelector } from 'react-redux';
 import StoreMallDirectoryOutlinedIcon from '@mui/icons-material/StoreMallDirectoryOutlined';
@@ -35,57 +35,35 @@ export default function SearchResult() {
     "availableEndTime": "2024-07-05T13:24:29.634Z"
   };
 
-  const {isLoading, data: establishmentSearchResult} = useQuery({queryKey: ['custom-data'], queryFn: () => { return endpoint.getEstablishmentSearch(payLoad)}});
+  const { isLoading, data: establishmentSearchResult } = useQuery({ queryKey: ['custom-data'], queryFn: () => endpoint.getEstablishmentSearch(payLoad) });
 
-  let establishmentList = useMemo(() => {
-    if (!isLoading) {
-      return establishmentSearchResult;
-    }
-    return null;
-  }, [isLoading]);
-
-  let establishmentResult = establishmentList != null ? establishmentList?.data?.data?.content : null;
+  const establishmentList = useMemo(() => {
+    return !isLoading ? establishmentSearchResult?.data?.data?.content : null;
+  }, [isLoading, establishmentSearchResult]);
 
   const [filteredData, setFilteredData] = useState<any[]>([]);
   const [isShowMap, setIsShowMap] = useState(false);
-  const FILTERR = useSelector((state: any) => {
-    return state.filterModal;
-  });
+  const FILTERR = useSelector((state: any) => state.filterModal);
 
   const sortBy = useCallback((list: any[]) => {
-
     if (FILTERR.SortBy === 'Price') {
-
-      // Sort function to sort services by price ascending
-      const sortByPrice = (a: any, b: any) => {
-        // Get the lowest price from each service array and compare them
-        const lowestPriceA = Math.min(...a.services?.map(service => service?.price));
-        const lowestPriceB = Math.min(...b.services?.map(service => service?.price));
+      list.sort((a, b) => {
+        const lowestPriceA = Math.min(...a.services.map(service => service?.price));
+        const lowestPriceB = Math.min(...b.services.map(service => service?.price));
         return lowestPriceA - lowestPriceB;
-      };
-
-      list.sort(sortByPrice);
-
+      });
     }
-
     if (FILTERR.SortBy === 'Ratings') {
-
-      // Sort function to sort by rating descending
-      const sortByRating = (a: any, b: any) => {
-        return b.rating - a.rating;
-      };
-
-      list.sort(sortByRating);
+      list.sort((a, b) => b.rating - a.rating);
     }
-
     return list;
-  }, [FILTERR.SortBy]);
+  }, [FILTERR]);
 
   useEffect(() => {
     if (!isLoading) {
-      let tempData: any[] = [...establishmentResult];
+      let tempData = [...establishmentList];
       if (FILTERR.selectedTags && FILTERR.selectedTags.length > 0) {
-        tempData = establishmentResult.filter((item: any) =>
+        tempData = establishmentList.filter((item: any) =>
           item.serviceTags.some((tag) =>
             FILTERR.selectedTags.some((selectedTag) =>
               tag.toLowerCase() === selectedTag.toLowerCase()
@@ -93,20 +71,18 @@ export default function SearchResult() {
           )
         );
       }
-
       if (FILTERR.price.min !== 0 || FILTERR.price.max !== 100) {
         tempData = tempData.map((item: any) => ({
           ...item,
-          services: item.services?.filter((service: any) =>
+          services: item.services.filter((service: any) =>
             service?.startingPrice >= FILTERR.price.min && service?.startingPrice <= FILTERR.price.max
           )
-        })).filter((item: any) => item.services?.length > 0); // Filter out items with empty service arrays
+        })).filter((item: any) => item.services.length > 0);
       }
-      
       tempData = sortBy(tempData);
       setFilteredData(tempData);
     }
-  }, [FILTERR, establishmentResult, establishmentList, sortBy]);
+  }, [FILTERR, establishmentList, isLoading, sortBy]);
 
   const StyledRating = styled(Rating)({
     '& .MuiRating-iconFilled': {
@@ -117,7 +93,9 @@ export default function SearchResult() {
     },
   });
 
-  function handleClick() {}
+  function handleClick() {
+    // Handle click logic for time slots if needed
+  }
 
   function handleMapClick() {
     setIsShowMap(prev => !prev);
@@ -127,130 +105,118 @@ export default function SearchResult() {
     return getRoute("SearchDetails");
   };
 
-  var settings1 = {
-    dots: false,
-    infinite: false,
-    speed: 500,
-    slidesToShow: 8,
-    slidesToScroll: 8,
-    initialSlide: 0,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
-  };
-
   return (
-    <Card className='search-page-container'>
-      <CardHeader style={{ marginTop: '60px', color: '#4D4D4D', backgroundColor: '#FFFBF3' }} id='card-header-id' title={filteredData.length + ' Venues matching your search'} action={
-        <div className='flex items-center'>
-          <FilterModal />
-          <div onClick={() => { handleMapClick() }}>Show map</div>
-        </div>
-      }>
-      </CardHeader>
+    <Card className='search-page-container' sx={{ width: '100%', height: '100%' }}>
+      <CardHeader
+        style={{ marginTop: '60px', color: '#4D4D4D', backgroundColor: '#FFFBF3' }}
+        id='card-header-id'
+        title={`${filteredData.length} Venues matching your search`}
+        action={
+          <div className='flex items-center'>
+            <FilterModal />
+            <div onClick={handleMapClick}>Show map</div>
+          </div>
+        }
+      />
       <hr />
       <div className='search-result-container'>
-        <div>
-          {filteredData && filteredData.length > 0 ? (
-            filteredData.map((card, index) => (
-              <CardContent key={card?.establishmentId} className='flex justify-center items-center'>
-                <Card sx={{ width: 1184 }} className='card-wrap-container'>
-                  <CardContent className='card-container'>
-                    <div className='card-header'>
-                      <GetImage imageName='SaloonImage' className='w-full rounded-lg' />
-                      <div className='card-header-details'>
-                        <div className='chip-wrap'>
-                          {card?.serviceTags?.map((tag, index) => (
-                            <Chip key={index} label={tag} className='mr-2 mb-2' />
-                          ))}
-                        </div>
-                        <div className='font-bold text-lg py-2' style={{ color: '#4D4D4D' }}>{card?.establishmentName}</div>
-                        <div className="card-rating">
-                          <div className='text-lg'>{card?.rating?.ratingStar}</div>
-                          <StyledRating
-                            name="customized-color"
-                            value={card?.rating?.ratingStar}
-                            precision={0.5}
-                            readOnly
-                          />
-                          <div className='text-sm font-bold'>{'(' + card?.rating?.ratingCount + ')'}</div>
-                        </div>
-                        <div className='text-base'>{card?.establishmentLocation}</div>
-                      </div>
-                    </div>
-                    <Grid>
-                      {card?.services?.map((service, index) => (
-                        <div className='card-body-details' key={index}>
-                          <div className='card-body-title'>
-                            <div className='font-semibold'>{service?.serviceName}</div>
-                            <div>from ${service?.startingPrice}</div>
-                          </div>
-                          <div className="card-slick-container">
-                            <div style={{ overflowX: 'auto', display: 'flex' }}>
-                              {service?.availabilities?.map((availability, index) => (
-                                <div key={index} className="availability-container">
-                                  {/* Display Date as TextField */}
-
-                                  {/* Display Time Slots as Chips */}
-                                  <div className="time-slots-container">
-                                    {availability.timeSlots.map((timeSlot, idx) => (
-                                      <Chip
-                                        key={idx}
-                                        label={timeSlot}
-                                        variant="outlined"
-                                        onClick={() => handleClick()}  // assuming handleClick needs both timeSlot and date
-                                        className="time-slot-chip"
-                                      />
-                                    ))}
-                                  </div>
-                                  <Typography sx={{color: '#B3B3B3', fontSize: '12px', fontWeight: '500', paddingLeft: '5px'}}>{availability?.date}</Typography>
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={isShowMap ? 6 : 12}>
+            <Grid container spacing={2}>
+              {filteredData && filteredData.length > 0 ? (
+                filteredData.map((card, index) => (
+                  <Grid item xs={12} key={card?.establishmentId}>
+                    <Card sx={{ width: '100%', height: '100%' }}>
+                      <CardContent>
+                        <div className='card-wrap-container'>
+                          <div className='card-container'>
+                            <div className='card-header'>
+                              <GetImage imageName='SaloonImage' className='w-full rounded-lg' />
+                              <div className='card-header-details'>
+                                <div className='chip-wrap'>
+                                  {card?.serviceTags?.map((tag, index) => (
+                                    <Chip key={index} label={tag} className='mr-2 mb-2' />
+                                  ))}
                                 </div>
-                              ))}
+                                <div className='font-bold text-lg py-2' style={{ color: '#4D4D4D' }}>{card?.establishmentName}</div>
+                                <div className="card-rating">
+                                  <div className='text-lg'>{card?.rating?.ratingStar}</div>
+                                  <StyledRating
+                                    name="customized-color"
+                                    value={card?.rating?.ratingStar}
+                                    precision={0.5}
+                                    readOnly
+                                  />
+                                  <div className='text-sm font-bold'>{'(' + card?.rating?.ratingCount + ')'}</div>
+                                </div>
+                                <div className='text-base'>{card?.establishmentLocation}</div>
+                              </div>
                             </div>
+                            <Grid container>
+                              {card?.services?.map((service, index) => (
+                                <Grid item xs={12} key={index}>
+                                  <div className='card-body-details'>
+                                    <div className='card-body-title'>
+                                      <div className='font-semibold'>{service?.serviceName}</div>
+                                      <div>from ${service?.startingPrice}</div>
+                                    </div>
+                                    <div className="card-slick-container">
+                                      <div style={{ overflowX: 'auto', display: 'flex' }}>
+                                        {service?.availabilities?.map((availability, index) => (
+                                          <div key={index} className="availability-container">
+                                            <div className="time-slots-container">
+                                              {availability.timeSlots.map((timeSlot, idx) => (
+                                                <Chip
+                                                  key={idx}
+                                                  label={timeSlot}
+                                                  variant="outlined"
+                                                  onClick={() => handleClick()}  // assuming handleClick needs both timeSlot and date
+                                                  className="time-slot-chip"
+                                                />
+                                              ))}
+                                            </div>
+                                            <Typography sx={{ color: '#B3B3B3', fontSize: '12px', fontWeight: '500', paddingLeft: '5px' }}>{availability?.date}</Typography>
+                                          </div>
+                                        ))}
+                                      </div>
+                                    </div>
+                                  </div>
+                                </Grid>
+                              ))}
+                            </Grid>
                           </div>
-
                         </div>
-                      ))}
-                    </Grid>
-                    <div className='flex gap-1'>view more <GetIcon className='' iconName='RightArrowIcon' /></div>
-                  </CardContent>
-                  <CardActions className='card-footer-action'>
-                    <StoreMallDirectoryOutlinedIcon />
-                    <TextRouter name={"Saloon Details"} to={`/salon/${card?.establishmentId}`} />
-                  </CardActions>
-                </Card>
-              </CardContent>
-            ))
-          ) : (
-            <h1 className='text-center align-middle'>No result found</h1>
-          )}
-        </div>
-        <div className='iframe-container'>
-          {isShowMap && <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31081.53269316962!2d80.20855351621644!3d13.15031202030962!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a5264db59c3d4b5%3A0x9be03109019f05f!2sMadhavaram%2C%20Chennai%2C%20Tamil%20Nadu!5e0!3m2!1sen!2sin!4v1716260701299!5m2!1sen!2sin" width="600" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>}
-        </div>
+                        <CardActions className='card-footer-action'>
+                          <StoreMallDirectoryOutlinedIcon />
+                          <TextRouter name={"Saloon Details"} to={`/salon/${card?.establishmentId}`} />
+                        </CardActions>
+                      </CardContent>
+                    </Card>
+                  </Grid>
+                ))
+              ) : (
+                <Grid item xs={12}>
+                  <Typography variant="h5" align="center" gutterBottom>No result found</Typography>
+                </Grid>
+              )}
+            </Grid>
+          </Grid>
+          <Grid item xs={12} md={isShowMap ? 6 : 0}>
+            <div className='iframe-container'>
+              {isShowMap && (
+                <iframe
+                  title="Google Map"
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31081.53269316962!2d80.20855351621644!3d13.15031202030962!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a5264db59c3d4b5%3A0x9be03109019f05f!2sMadhavaram%2C%20Chennai%2C%20Tamil%20Nadu!5e0!3m2!1sen!2sin!4v1716260701299!5m2!1sen!2sin"
+                  width="100%"
+                  height="450"
+                  allowFullScreen=""
+                  loading="lazy"
+                  referrerPolicy="no-referrer-when-downgrade"
+                ></iframe>
+              )}
+            </div>
+          </Grid>
+        </Grid>
       </div>
     </Card>
   );
