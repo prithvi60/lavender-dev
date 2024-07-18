deca56b51b1d841f5c89421905e8e74cc0dbac55
vamsi <156523177+venkata-vamsi@users.noreply.github.com>
2024-07-18 20:44:28 +0530

    fix

--- src/features/Login/index.js
+++ src/features/Login/index.js
@@ -2,53 +2,43 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { Paper, Grid, useMediaQuery, useTheme, IconButton, Box } from '@mui/material';
 import Text from '../../components/Text';
-import { Padding } from '@mui/icons-material';
 import GetIcon from '../../assets/Icon/icon';
 import { useNavigate } from 'react-router-dom';
 
 const Login = ({children}) => {
     const { newAccount, accountCreated } = useSelector((state) => state.loginPage);
 
-    const navigate = useNavigate()
+    const navigate = useNavigate();
     const theme = useTheme();
     const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
 
     return (
-        <>
-            <Grid container spacing={5} justifyContent="center" alignItems="center" className="login-container">
-                <Grid item xs={12}>
-                    <Grid container justifyContent="center" alignItems="center">
-                        {
-                            !isSmallScreen &&
-                            <Grid xs={4} sx={{display: 'flex', flexDirection: 'column', justifyContent: 'space-between', paddingBottom: '50px'}}>
-                                    <Box sx={{paddingBottom: '100px', paddingLeft: '10px'}}>
-                                        <IconButton onClick={() =>{ navigate('/')}}>
-                                            <GetIcon sx={{cursor: 'pointer'}} iconName='BackIconWhite' />
-                                        </IconButton>
-                                    </Box>
-                                    
-                                    
-                                    <Box sx={{paddingTop: '10px'}}>
-                                        <Text name={'Discover me time'} sx={styles.title} align={'center'}>Discover me time</Text>
-                                        <Text name={'Book your next salon experience with Lavender'} sx={styles.subtitle} align={'right'}></Text>
-                                    </Box>
-
+        <Grid container spacing={5} justifyContent="center" alignItems="center" className="login-container">
+            <Grid item xs={12}>
+                <Grid container justifyContent="center" alignItems="center">
+                    {!isSmallScreen && (
+                        <Grid item xs={5} sx={{ position: 'relative', top: '-20px', minHeight: '500px', display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+                            <Grid container alignItems="center" justifyContent="center">
+                                <IconButton onClick={() => navigate('/')} sx={{ position: 'absolute', top: 0, left: 0 }}>
+                                    <GetIcon sx={{ cursor: 'pointer' }} iconName='BackIconWhite' />
+                                </IconButton>
                             </Grid>
-                        }
-
-                        <Grid item xs={!isSmallScreen ? 8 : 12}>
-                            <Paper elevation={3} className='login-subcontainer' >
-                                <main>{children}</main>
-                                {/* {!newAccount && !accountCreated && <LoginLanding />}
-                                {newAccount && <RegisterSide />}
-                                {accountCreated && <SingInSide />} */}
-                            </Paper>
+                            <Box sx={{ paddingTop: '10px' }}>
+                                <Text name={'Discover me time'} sx={styles.title} align={'right'}>Discover me time</Text>
+                                <Text name={'Book your next salon experience with Lavender'} sx={styles.subtitle} align={'right'}></Text>
+                            </Box>
                         </Grid>
+                    )}
+
+                    <Grid item xs={!isSmallScreen ? 7 : 12}>
+                        <Paper elevation={3} className='login-subcontainer'>
+                            <main>{children}</main>
+                            {/* Render components based on conditions */}
+                        </Paper>
                     </Grid>
                 </Grid>
             </Grid>
-        </>
-        
+        </Grid>
     );
 };
 
@@ -68,4 +58,4 @@ const styles = {
         color: "#FFFFFF",
         lineHeight: '38px'
     }
-}
+};
