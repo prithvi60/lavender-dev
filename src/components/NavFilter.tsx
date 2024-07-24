9497fe017394960e5bd311507775b69d64b73617
rajesh-orrizonte <rajesh.pilli@orrizonte.in>
2024-07-24 23:17:31 +0530

    nav filter changes done

--- src/components/NavFilter.tsx
+++ src/components/NavFilter.tsx
@@ -1,31 +1,38 @@
-import React from 'react';
+import React from "react";
 import { Search } from "@mui/icons-material";
-import { useSelector } from 'react-redux';
-import { convertTo_HH_AM } from '../utils/TimeFormat.ts';
+import { useSelector } from "react-redux";
+import { convertTo_HH_AM } from "../utils/TimeFormat.ts";
 
-const NavFilter = ({setshowSearchBar}) => {
+const NavFilter = ({ setshowSearchBar }) => {
+  const { treatmentList, locationList, selectedDate, SelectedTime } =
+    useSelector((state: any) => state.searchPage);
 
-  const { treatmentList, locationList, selectedDate, SelectedTime } = useSelector(
-    (state: any) => state.searchPage
-  );
-
-  const onClickHandle = () =>{
-    setshowSearchBar(prev=> !prev)
-  }
+  const onClickHandle = () => {
+    setshowSearchBar((prev) => !prev);
+  };
 
   return (
-    <div id='navdiv' className='filtered-panel ml-auto cursor-pointer' onClick={()=> onClickHandle()}>
-      <div className='filtered-items'>
-        <p>{treatmentList?.toString().replaceAll(',', ', ')}</p>
+    <div
+      id="navdiv"
+      className="filtered-panel ml-auto cursor-pointer"
+      onClick={() => onClickHandle()}
+    >
+      <div className="filtered-items">
+        <p className="m-0">{treatmentList?.toString().replaceAll(",", ", ")}</p>
       </div>
-      <div className='filtered-items'>
-        <p>{locationList?.toString().replaceAll(',', ', ')}</p>
+      <div className="filtered-items">
+        <p className="m-0">
+          {locationList[0]?.location?.toString().replaceAll(",", ", ")}
+        </p>
       </div>
-      <div className='filtered-items'>
-        <p>{selectedDate}</p>
+      <div className="filtered-items">
+        <p className="m-0">{selectedDate}</p>
       </div>
-      <div className='filtered-items'>
-        <p>{ }</p> <div className='icon-wrapper'><Search /></div>
+      <div className="filtered-items">
+        <p className="m-0">{`${SelectedTime?.from} - ${SelectedTime?.to}`}</p>{" "}
+        <div className="icon-wrapper">
+          <Search />
+        </div>
       </div>
     </div>
   );
